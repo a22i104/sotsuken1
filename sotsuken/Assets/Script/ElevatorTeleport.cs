4343f20534b4780aec10252a7fd562466241a48e
a22i104 <a22i104ts@ous.jp>
2025-12-16 03:30:57 +0900

    update12/16-hm

--- sotsuken/Assets/Script/ElevatorTeleport.cs
+++ sotsuken/Assets/Script/ElevatorTeleport.cs
@@ -1,73 +1,21 @@
 using UnityEngine;
-using UnityEngine.AI;
 
 public class ElevatorTeleport : MonoBehaviour
 {
-    private NavMeshAgent agent;
-    private CharacterController controller;
-    private bool isTeleporting = false;
+    public Transform teleportTarget;
 
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        agent = GetComponent<NavMeshAgent>();
-        controller = GetComponent<CharacterController>();
+        if (!other.CompareTag("Player")) return;
 
-        NavMeshHit hit;
-        if (NavMesh.SamplePosition(transform.position, out hit, 2.0f, NavMesh.AllAreas))
-        {
-            agent.Warp(hit.position);
-        }
-        else
-        {
-            Debug.LogError("NavMeshが近くにありません！");
-        }
-    }
+        CharacterController cc = other.GetComponent<CharacterController>();
 
+        if (cc != null)
+            cc.enabled = false;
 
-    void LateUpdate()
-    {
-        if (agent.isOnNavMesh)
-        {
-            agent.nextPosition = transform.position;
-        }
-        else
-        {
-            NavMeshHit hit;
-            if (NavMesh.SamplePosition(transform.position, out hit, 1.0f, NavMesh.AllAreas))
-            {
-                agent.Warp(hit.position);
-            }
-        }
-    }
+        other.transform.position = teleportTarget.position;
 
-
-    void Update()
-    {
-        Debug.Log("isOnOffMeshLink: " + agent.isOnOffMeshLink);
-
-        if (agent.isOnOffMeshLink && !isTeleporting)
-        {
-            Teleport();
-        }
-    }
-
-    void Teleport()
-    {
-        isTeleporting = true;
-
-        OffMeshLinkData data = agent.currentOffMeshLinkData;
-
-        // CharacterControllerを一時停止
-        controller.enabled = false;
-
-        // プレイヤー本体を出口へ瞬間移動
-        transform.position = data.endPos;
-
-        // Agentも同期
-        agent.Warp(data.endPos);
-        agent.CompleteOffMeshLink();
-
-        controller.enabled = true;
-        isTeleporting = false;
+        if (cc != null)
+            cc.enabled = true;
     }
 }
