faa2dea2182e24b5b21d2c9020f5ac70625ac460
a22i104 <a22i104ts@ous.jp>
2025-12-16 15:53:14 +0900

    update12/16-sh

--- sotsuken/Assets/Script/PlayerController.cs
+++ sotsuken/Assets/Script/PlayerController.cs
@@ -1,49 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [Header("Movement")]
-    public float moveSpeed = 5f;
-    public float jumpForce = 5f;
-    public float groundDrag = 5f;
+    [Header("Ground Check")]
+    [Header("Key")]
+    public KeyCode _jumpKey = KeyCode.Space;
+    public KeyCode _DashKey = KeyCode.LeftShift;
+    public KeyCode _abilityKey = KeyCode.E;
+    public Coroutine SpeedBoostCoroutine;
+    public Coroutine JumpBoostCoroutine;
+    private float _moveSpeed;
+    public float _walkSpeed;
+    public float _DashSpeed;
+    public float _playerHeight;
+    public float _jump;
+    public float _jumpCooldown;
+    public float _Sdct;
+    public float _Sjct;
+    public float _count;
 
-    [Header("Keys")]
-    public KeyCode jumpKey = KeyCode.Space;
+    public float _airMultiplier;
+    public float _groundDrag;
 
-    [Header("Check")]
-    public float playerHeight = 2f;
     public LayerMask whatIsGround;
+    public LayerMask whatIsWall;
+    public Transform _orientation;
 
+    float _horizontalInput;
+    float _verticalInput;
+    public bool isTakeing = false;
+   
+
+    private bool isJumpReady;
+    private bool isGrounded;
+    Vector3 moveDirection;
     Rigidbody rb;
-    bool isGrounded;
-    float horizontal;
-    float vertical;
-    public Transform orientation;
+    
+
+    public MovementState state;
+    public MovementState item_state;
+
+    public enum MovementState
+    {
+        def,
+        Walking,
+        Dashing,
+        Brinking,
+        SuperJumping,
+        SuperDashing,
+        Interrupting,
+        JumpingNow
+    }
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
-    }
+        isJumpReady = true;
+       
+        _Sdct = 1.0f;
+        _Sjct = 1.0f;
 
-    void Update()
-    {
-        // 地面チェック
-        isGrounded = Physics.Raycast(transform.position, Vector3.down,
-            playerHeight * 0.5f + 0.2f, whatIsGround);
-
-        // Drag設定
-        rb.drag = isGrounded ? groundDrag : 0;
-
-        // 入力取得
-        horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("Vertical");
-
-        // ジャンプ
-        if (isGrounded && Input.GetKey(jumpKey))
-        {
-            Jump();
-        }
     }
 
     void FixedUpdate()
@@ -51,15 +71,90 @@ public class PlayerController : MonoBehaviour
         Move();
     }
 
+    void Update()
+    {
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, _playerHeight * 0.5f + 0.2f, whatIsGround);
+
+        Debug.DrawRay(transform.position, Vector3.down * (_playerHeight * 0.5f + 0.2f), Color.red);
+
+        rb.drag = isGrounded ? _groundDrag : 0;
+
+        HandleInput();
+        LimitSpeed();
+        StateHandler();
+       
+    }
+
+    void HandleInput()
+    {
+        _horizontalInput = Input.GetAxisRaw("Horizontal");
+        _verticalInput = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey(_jumpKey) && isJumpReady && isGrounded)
+        {
+            isJumpReady = false;
+            Jump();
+            Invoke(nameof(ResetJump), _jumpCooldown);
+        }
+    }
+
+    void StateHandler()
+    {
+        if (isGrounded && Input.GetKey(_DashKey))
+        {
+            state = MovementState.Dashing;
+            _moveSpeed = _DashSpeed;
+        }
+        else if (isGrounded)
+        {
+            state = MovementState.Walking;
+            _moveSpeed = _walkSpeed;
+        }
+        else
+        {
+            state = MovementState.JumpingNow;
+        }
+    }
+
+   
+
     void Move()
     {
-        Vector3 direction = orientation.forward * vertical + orientation.right * horizontal;
-        rb.AddForce(direction.normalized * moveSpeed * 10f, ForceMode.Force);
+        moveDirection = _orientation.forward * _verticalInput + _orientation.right * _horizontalInput;
+
+        if (isGrounded)
+        {
+            rb.AddForce(moveDirection.normalized * _moveSpeed * 10f, ForceMode.Force);
+        }
+        else
+        {
+            rb.AddForce(moveDirection.normalized * _moveSpeed * 10f * _airMultiplier, ForceMode.Force);
+        }
+    }
+
+    void LimitSpeed()
+    {
+        Vector3 flat = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        if (flat.magnitude > _moveSpeed)
+        {
+            Vector3 limit = flat.normalized * _moveSpeed;
+            rb.velocity = new Vector3(limit.x, rb.velocity.y, limit.z);
+        }
     }
 
     void Jump()
     {
         rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        rb.AddForce(transform.up * _jump, ForceMode.Impulse);
     }
+
+    void ResetJump()
+    {
+        isJumpReady = true;
+    }
+
+    
+
+
+    
 }
